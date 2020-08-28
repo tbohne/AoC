c9156c8d08d36f255108b42891665f1cdf04b87f
tbohne <tbohne@uni-osnabrueck.de>
2020-08-28 15:59:58 +0200

    minor changes

--- 2015/d1.py
+++ 2015/d1.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
-from typing import Optional, Tuple
+from typing import Tuple
 
 
-def solve(data: str) -> Tuple[int, Optional[int]]:
+def solve(data: str) -> Tuple[int, int]:
     floor = 0
-    basement = None
+    basement = -1
     for pos, direction in enumerate(data, 1):
         if direction == "(":
             floor += 1
         elif direction == ")":
             floor -= 1
-        if floor == -1 and basement is None:
+        if floor == -1 and basement == -1:
             basement = pos
     return floor, basement
 
 
 if __name__ == '__main__':
-    with open('input.txt', 'r') as file:
+    with open('input1.txt', 'r') as file:
         data = file.read().replace('\n', '')
     floor, first_basement = solve(data)
     print("part one:", floor)
