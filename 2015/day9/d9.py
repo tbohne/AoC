fef5c0d80024b3987a35a87310aaf85b559ae8fc
tbohne <tbohne@uni-osnabrueck.de>
2021-06-25 20:24:43 +0200

    minor changes

--- 2015/day9/d9.py
+++ 2015/day9/d9.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 import numpy as np
-import math
 import itertools
 
 TEST = ["London to Dublin = 464", "London to Belfast = 518", "Dublin to Belfast = 141"]
 
 
-def create_city_dict(data):
+def create_city_dict(data: list) -> dict:
     idx = 0
     idx_to_city = {}
     for l in data:
@@ -21,36 +20,28 @@ def create_city_dict(data):
     return idx_to_city
 
 
-def create_distances(data, city_dict):
+def create_distances(data: list, city_dict: dict) -> np.array:
     dist = np.empty((len(city_dict), len(city_dict)))
     for l in data:
         c1, _, c2, _, d = l.split(" ")
-        dist[city_dict[c1]][city_dict[c2]] = d
-        dist[city_dict[c2]][city_dict[c1]] = d
+        dist[city_dict[c1]][city_dict[c2]] = dist[city_dict[c2]][city_dict[c1]] = d
     return dist
 
 
-def solve(data):
-    city_dict = create_city_dict(data)
-    dist = create_distances(data, city_dict)
-    shortest = math.inf
-    longest = -math.inf
-    for perm in list(itertools.permutations(city_dict.keys())):
-        d = 0
-        for loc in range(len(perm) - 1):
-            d += dist[city_dict[perm[loc]]][city_dict[perm[loc + 1]]]
-        if d < shortest:
-            shortest = d
-        if d > longest:
-            longest = d
-    return shortest, longest
+def solve(data: list) -> (int, int):
+    d = create_city_dict(data)
+    dist = create_distances(data, d)
+    permutations = list(itertools.permutations(d.keys()))
+    d = [sum([dist[d[perm[loc]]][d[perm[loc + 1]]] for loc in range(len(perm) - 1)]) for perm in permutations]
+    return min(d), max(d)
 
 
 if __name__ == '__main__':
+    assert solve(TEST)[0] == 605
     with open('in9.txt', 'r') as file:
         lines = [l.strip() for l in file.readlines()]
-        assert solve(TEST)[0] == 605
-        assert solve(lines)[0] == 207
-        assert solve(lines)[1] == 804
-        print("p1:", solve(lines)[0])
-        print("p2:", solve(lines)[1])
+        p1, p2 = solve(lines)
+        assert p1 == 207
+        assert p2 == 804
+        print("p1:", p1)
+        print("p2:", p2)
