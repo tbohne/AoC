62cedcc9a8c65c7c9bb2b751be865face123815c
tbohne <tbohne@uni-osnabrueck.de>
2021-06-25 20:02:19 +0200

    2015 - d9 - p2

--- 2015/day9/d9.py
+++ 2015/day9/d9.py
@@ -11,12 +11,11 @@ def create_city_dict(data):
     idx = 0
     idx_to_city = {}
     for l in data:
-        c1 = l.split(" ")[0]
-        c2 = l.split(" ")[2]
-        if not c1 in idx_to_city:
+        c1, _, c2, _, _ = l.split(" ")
+        if c1 not in idx_to_city:
             idx_to_city[c1] = idx
             idx += 1
-        if not c2 in idx_to_city:
+        if c2 not in idx_to_city:
             idx_to_city[c2] = idx
             idx += 1
     return idx_to_city
@@ -31,23 +30,27 @@ def create_distances(data, city_dict):
     return dist
 
 
-def part_one(data):
+def solve(data):
     city_dict = create_city_dict(data)
     dist = create_distances(data, city_dict)
     shortest = math.inf
+    longest = -math.inf
     for perm in list(itertools.permutations(city_dict.keys())):
         d = 0
         for loc in range(len(perm) - 1):
             d += dist[city_dict[perm[loc]]][city_dict[perm[loc + 1]]]
         if d < shortest:
             shortest = d
-    return shortest
+        if d > longest:
+            longest = d
+    return shortest, longest
 
 
 if __name__ == '__main__':
     with open('in9.txt', 'r') as file:
-        data = [l.strip() for l in file.readlines()]
-        assert part_one(TEST) == 605
-        assert part_one(data) == 207
-        print("p1:", part_one(data))
-
+        lines = [l.strip() for l in file.readlines()]
+        assert solve(TEST)[0] == 605
+        assert solve(lines)[0] == 207
+        assert solve(lines)[1] == 804
+        print("p1:", solve(lines)[0])
+        print("p2:", solve(lines)[1])
