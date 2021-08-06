aa4daa6215fa889c1c96345cfdd4dae1ff9d71e0
tbohne <tbohne@uni-osnabrueck.de>
2021-08-06 15:21:18 +0200

    2015 d12 p2

--- 2015/day12/d12.py
+++ 2015/day12/d12.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 
 import json
+import re
 
 
-def print_vals(dic):
+def solve(dic, part_one):
     s = 0
     if isinstance(dic, dict):
+        if not part_one and "red" in str(dic.values()):
+            tst = str(dic)
+            for i in [m.start() for m in re.finditer("red", tst)]:
+                curly_sum = tst[:i].count("{") - tst[:i].count("}")
+                square_sum = tst[:i].count("[") - tst[:i].count("]")
+                if curly_sum == 1 and square_sum == 0:
+                    return 0
         for key in dic.keys():
             if isinstance(dic[key], dict):
-                s += print_vals(dic[key])
+                s += solve(dic[key], part_one)
             elif isinstance(dic[key], list):
-                s += print_vals(dic[key])
+                s += solve(dic[key], part_one)
             else:
                 if str(dic[key]).lstrip("-").isdigit():
                     s += int(dic[key])
     elif isinstance(dic, list):
         for i in dic:
             if isinstance(i, dict):
-                s += print_vals(i)
+                s += solve(i, part_one)
             elif isinstance(i, list):
-                s += print_vals(i)
+                s += solve(i, part_one)
             else:
                 if str(i).lstrip("-").isdigit():
                     s += int(i)
@@ -32,5 +40,9 @@ def print_vals(dic):
 if __name__ == '__main__':
     with open('in12.txt', 'r') as file:
         data = json.loads(file.read())
-        p1 = sum([print_vals(val) for _, val in data.items()])
+        p1 = sum([solve(val, True) for _, val in data.items()])
+        p2 = sum([solve(val, False) for _, val in data.items()])
         assert p1 == 119433
+        assert p2 == 68466
+        print("p1:", p1)
+        print("p2:", p2)
