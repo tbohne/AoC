83d5f4316d92455797d6503426b5642cad5b2245
tbohne <tbohne@uni-osnabrueck.de>
2021-09-08 22:05:13 +0200

    2015 - d15 - p2

--- 2015/d15.py
+++ 2015/d15.py
@@ -3,6 +3,8 @@ import fileinput
 import itertools
 
 LIMIT = 100
+CALORIES = 500
+
 
 class Ingredient:
 
@@ -14,9 +16,6 @@ class Ingredient:
         self.texture = texture
         self.calories = calories
 
-    def __str__(self):
-        return f'{self.name}: cap: {self.capacity}, dur: {self.durability}, flav: {self.flavor}, text: {self.texture}, cal: {self.calories}'
-
 
 def parse(line):
     name = line.split(":")[0]
@@ -24,21 +23,21 @@ def parse(line):
     return name, *[int(prop.strip().split(" ")[1]) for prop in properties]
 
 
-def part_one(ingredients, combinations):
+def solve(ingredients, combinations, part_one):
     res = 0
     for c in combinations:
-        cap = max(0, sum([c[i] * ingredients[i].capacity for i in range(len(c))]))
-        dur = max(0, sum([c[i] * ingredients[i].durability for i in range(len(c))]))
-        flav = max(0, sum([c[i] * ingredients[i].flavor for i in range(len(c))]))
-        text = max(0, sum([c[i] * ingredients[i].texture for i in range(len(c))]))
-        mul = cap * dur * flav * text
-        if mul > res:
+        capacity = max(0, sum([c[i] * ingredients[i].capacity for i in range(len(c))]))
+        durrablity = max(0, sum([c[i] * ingredients[i].durability for i in range(len(c))]))
+        flavor = max(0, sum([c[i] * ingredients[i].flavor for i in range(len(c))]))
+        texture = max(0, sum([c[i] * ingredients[i].texture for i in range(len(c))]))
+        calories = max(0, sum([c[i] * ingredients[i].calories for i in range(len(c))]))
+        mul = capacity * durrablity * flavor * texture
+        if mul > res and (part_one or calories == CALORIES):
             res = mul
     return res
 
 
 if __name__ == '__main__':
-
     data = [line.strip() for line in fileinput.input()]
     ingredients = []
     for d in data:
@@ -46,7 +45,9 @@ if __name__ == '__main__':
 
     nums = [i for i in range(1, LIMIT + 1)]
     combinations = [i for i in list(itertools.permutations(nums, len(ingredients))) if sum(i) == LIMIT]
-    p1 = part_one(ingredients, combinations)
+    p1 = solve(ingredients, combinations, True)
+    p2 = solve(ingredients, combinations, False)
     assert p1 == 18965440
+    assert p2 == 15862900
     print("p1:", p1)
-
+    print("p2:", p2)
