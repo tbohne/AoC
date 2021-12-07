ebc110a96583987eac54700a029d4c32f720783a
tbohne <tbohne@uni-osnabrueck.de>
2021-12-07 08:46:59 +0100

    minor changes

--- 2021/d7.py
+++ 2021/d7.py
@@ -3,10 +3,10 @@
 import fileinput
 
 
-def comp_costs(p1, p2):
+def comp_costs(diff):
     fuel = 1
     costs = 0
-    for i in range(abs(p1 - p2)):
+    for _ in range(diff):
         costs += fuel
         fuel += 1
     return costs
@@ -15,15 +15,14 @@ def comp_costs(p1, p2):
 def solve(positions, d1):
     fuels = []
     for pos in range(min(positions), max(positions)):
-        fuels.append(sum([comp_costs(pos, goal_pos) if not d1 else abs(pos - goal_pos) for goal_pos in positions]))
+        fuels.append(sum([comp_costs(abs(pos - goal_pos)) if not d1 else abs(pos - goal_pos) for goal_pos in positions]))
     return min(fuels)
 
 
 if __name__ == '__main__':
     data = [int(i) for i in "".join([l.strip() for l in fileinput.input()]).split(",")]
     p1 = solve(data, True)
-    assert p1 == 337833
     p2 = solve(data, False)
-
+    assert p1 == 337833
     assert p2 == 96678050
     print("p1:", p1, "p2:", p2)
