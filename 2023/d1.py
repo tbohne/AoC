8a4ce85b43578e04921c29a65d2cd98eee934f8a
tbohne <tbohne@uni-osnabrueck.de>
2023-12-14 22:56:37 +0100

    clean up 2023 - d1

--- 2023/d1.py
+++ 2023/d1.py
@@ -2,22 +2,24 @@
 
 import fileinput
 
-def p1(day_in):
-    digits = [[c for c in code if c.isdigit()] for code in day_in]
+
+def compute_calibration_val(cal_lines):
+    digits = [[c for c in line if c.isdigit()] for line in cal_lines]
     return sum([int(d[0] + d[-1]) for d in digits])
 
-def p2(day_input):
-    digit_str = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9 }
+
+def p2(cal_lines):
+    digit_str = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
     for i in range(len(day_in)):
         for k in digit_str.keys():
-            if k in day_input[i]:
-                day_input[i] = day_input[i].replace(k, k[0] + str(digit_str[k]) + k[-1])
-
-    return p1(day_input)
-
+            if k in cal_lines[i]:
+                cal_lines[i] = cal_lines[i].replace(k, k[0] + str(digit_str[k]) + k[-1])
+    return compute_calibration_val(cal_lines)
 
 
 if __name__ == '__main__':
     day_in = list(fileinput.input())
-    assert p1(day_in) == 54632
-    assert p2(day_in) == 54019
+    p1 = compute_calibration_val(day_in)
+    p2 = p2(day_in)
+    assert p1 == 54632
+    assert p2 == 54019
